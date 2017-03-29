bfcf0c91c684a9508793cf753e82407510310093
宝码 <nongyoubao@alibaba-inc.com>
2017-03-29 20:58:29 +0800

    test: eslint standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,7 @@
 module.exports = {
-  "extends": "airbnb-base",
+  "extends": "standard",
   "plugins": [
-    "import"
-  ],
-  "env": {
-    "mocha": true
-  },
-  "rules": {
-    "strict": "off",
-    "comma-dangle": ["error", "never"],
-    "no-console": "off",
-    "no-param-reassign": "off",
-    "no-cond-assign": "off"
-  }
+    "standard",
+    "promise"
+  ]
 };
--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-'use strict';
-
-module.exports = require('./src/md2jsonml');
+module.exports = require('./src/md2jsonml')
